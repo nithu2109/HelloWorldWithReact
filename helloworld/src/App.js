3bf07347098e2a4522e5b02251fc4cd86614ae74
nithu2109 <67360225+nithu2109@users.noreply.github.com>
2021-07-13 22:06:48 +0530

    Add files via upload

---
+++ helloworld/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function Apple(){
+    return (<div>
+        <h1>Hello World!</h1>
+        <h3>Very First React Project!</h3>
+    </div>);
+
+}
+
+export default Apple;
